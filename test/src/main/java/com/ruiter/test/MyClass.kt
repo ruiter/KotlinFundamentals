104ab09b5088326b1d2334b7901d13b66a96063a
Ruiter Matos <ruitercomp@gmail.com>
2022-01-06 09:29:38 -0300

    hello world

---
+++ test/src/main/java/com/ruiter/test/MyClass.kt
@@ -0,0 +1,13 @@
+package com.ruiter.test
+
+fun main() {
+    val myName = "Ruiter"
+
+    println(myName)
+
+    val eDouble = 3.12345556665
+    val eFloat = 3.12345556665f
+
+    println(eDouble)
+    println(eFloat)
+}
